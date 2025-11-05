17ee64cd745840a24f903acf4c72d75826662be6
void627 <OIKHJGpoikjh@proton.me>
2025-11-05 22:54:22 +0400

    initial commit

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# Default build type is Debug
+BUILD_TYPE=${1:-Debug}
+
+# Normalize capitalization (e.g., debug → Debug)
+BUILD_TYPE=$(echo "$BUILD_TYPE" | awk '{print toupper(substr($0,1,1)) tolower(substr($0,2))}')
+
+echo "Building in $BUILD_TYPE mode..."
+
+mkdir -p build
+cd build
+
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ..
+make -j$(nproc)
+
+EXEC="./bin/$BUILD_TYPE/raylib_project"
+
+if [ -f "$EXEC" ]; then
+    echo "Running $EXEC"
+    $EXEC
+else
+    echo "Executable not found: $EXEC"
+fi
